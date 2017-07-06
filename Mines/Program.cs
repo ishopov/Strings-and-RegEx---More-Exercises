bc9d96cf4401d0a6e224628766aa9f13ad20f126
ishopov <ivo.shopov.1990@gmail.com>
2017-07-06 11:36:33 +0300

    Add files via upload

---
+++ Mines/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Mines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mineField = Console.ReadLine();
+            var pattern = "<(?<first>.)(?<second>.)>";
+            var matches = Regex.Matches(mineField, pattern);
+            foreach (Match mine in matches)
+            {
+                var firstChar = mine.Groups["first"].Value;
+                var secondChar = mine.Groups["second"].Value;
+                var blastRadius = Math.Abs(firstChar[0] - secondChar[0]);
+                pattern = MinesPattern(blastRadius, firstChar, secondChar);
+                var match = Regex.Match(mineField, pattern).Value;
+                mineField = mineField.Replace(match, new string('_', match.Length));
+            }
+
+            Console.WriteLine(mineField);
+        }
+
+        static string MinesPattern(int blastRadius, string firstChar, string secondChar)
+        {
+            var specialChars = @"/.*+?|(,)[]{}\";
+            firstChar = specialChars.Contains(firstChar) ? @"\" + firstChar : firstChar;
+            secondChar = specialChars.Contains(secondChar) ? @"\" + secondChar : secondChar;
+            return $".{{0,{blastRadius}}}<(?<first>{firstChar})(?<second>{secondChar})>.{{0,{blastRadius}}}";
+        }
+    }
+}
